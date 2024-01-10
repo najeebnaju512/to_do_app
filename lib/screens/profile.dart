ae3b16abb567012e590e2d3a0bfb7cb9328e3734
najeebnaju512 <najeebnaju512@gmail.com>
2024-01-10 20:20:36 +0530

    8/20--10/1/24

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -3,8 +3,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:to_do_app/utils/colors.dart';
 
 class Profile extends StatelessWidget {
-  const Profile({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,71 +20,97 @@ class Profile extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 50),
-            child: Center(
-              child: Column(
-                children: [
-                  Container(
-                    height: 120,
-                    width: 120,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black45,
-                          blurRadius: 5.0,
-                          spreadRadius: 1.0,
-                          offset: Offset(
-                              2.0, 2.0), // shadow direction: bottom right
-                        )
-                      ],
-                    ),
-                    child: CircleAvatar(
-                      child: Icon(
-                        Icons.person,
-                        size: 40,
+          Card(
+            color: primethemeClr,
+            elevation: 0,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: Center(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 120,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black45,
+                            blurRadius: 5.0,
+                            spreadRadius: 1.0,
+                            offset: Offset(
+                                2.0, 2.0), // shadow direction: bottom right
+                          )
+                        ],
+                      ),
+                      child: CircleAvatar(
+                        child: Icon(
+                          Icons.person,
+                          size: 40,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    "UserName",
-                    style: GoogleFonts.pacifico(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w300,
-                        color: thirdthemeClr),
-                  ),
-                  Text("email*****@gmail.com",
-                      style: GoogleFonts.lora(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                          color: secndthemeClr)),
-                ],
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      "UserName",
+                      style: GoogleFonts.pacifico(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w300,
+                          color: thirdthemeClr),
+                    ),
+                    Text("email*****@gmail.com",
+                        style: GoogleFonts.lora(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400,
+                            color: secndthemeClr)),
+                  ],
+                ),
               ),
             ),
           ),
-          // Container(
-                  //   child: ListView(
-                  //     children: [
-                  //       ListTile(
-                  //         //privacy
-                  //         leading: Icon(Icons.privacy_tip_outlined,
-                  //         size: 20,
-                  //         ),
-                  //         title: Text("Privacy"),
-                  //         trailing: Icon(Icons.arrow_forward_ios),
-                  //       )
-                  //       //help&support
-                  //       //settings
-                  //       //share
-                  //     ],
-                  //   ),
-                  // )
+          Container(
+            child: Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView(
+                  children: [
+                    buildListTile(Icons.privacy_tip_outlined, "Privacy"),
+                    buildListTile(Icons.help_outline, "Help&Support"),
+                    buildListTile(Icons.settings, "Settings"),
+                    buildListTile(Icons.share, "Share"),
+                  ],
+                ),
+              ),
+            ),
+          )
         ],
       ),
+    );
+  }
+
+  buildListTile(IconData iconname, String title) {
+    return Column(
+      children: [
+        ListTile(
+          contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          titleAlignment: ListTileTitleAlignment.center,
+          tileColor: Colors.white38,
+          leading: Icon(iconname, size: 25),
+          title: Text(
+            title,
+            style: GoogleFonts.robotoSlab(fontSize: 20, fontWeight: FontWeight.w400),
+          ),
+          trailing: Icon(Icons.arrow_forward_ios),
+        ),
+        SizedBox(
+          height: 10,
+        )
+      ],
     );
   }
 }
